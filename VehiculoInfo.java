17516f6856f55596753242e281a5968a6a869f25
OowarriorwhiteoO <warriorwhite@gmail.com>
2025-08-31 23:03:30 -0400

    Etapa 2 — captura de datos del vehículo (marca, modelo, combustible, cilindrada y capacidad pasajeros)

--- VehiculoInfo.java
+++ VehiculoInfo.java
@@ -14,11 +14,49 @@ public class VehiculoInfo {
         // se muestra mensaje pidiendo el nombre del usuario
         System.out.println("Ingrese su nombre:");
         
-        // se lee una línea completa de texto y se almacena en la variable nombre
+        // se guarda una línea completa de texto y se almacena en la variable nombre
         String nombre = scanner.nextLine();
         
         // se muestra mensaje pidiendo la edad del usuario
         System.out.println("Ingrese su edad:");
         
-        // selee un número entero y lo almacena en la variable edad
+        // seguarda un número entero y lo almacena en la variable edad
         int edad = scanner.nextInt();
+
+        // Limpia el buffer del scanner (consume el salto de línea pendiente después de nextInt())
+        scanner.nextLine(); // limpiar salto
+        
+        // Caso: datos del vehículo
+        //  Se solicita la marca del vehículo
+        System.out.println("Ingrese la marca del vehiculo:");
+        
+        // Se guarda la marca del vehículo como string
+        String marca = scanner.nextLine();
+        
+        // Se solicita el modelo del vehículo
+        System.out.println("Ingrese el modelo del vehiculo:");
+        
+        // guarda el modelo del vehículo como string
+        String modelo = scanner.nextLine();
+        
+        // Solicita el tipo de combustible
+        System.out.println("Ingrese el tipo de combustible:");
+        
+        // guarda el tipo de combustible como string
+        String tipoCombustible = scanner.nextLine();
+        
+        // se solicita la cilindrada con ejemplo
+        System.out.println("Ingrese la cilindrada (ej: 1600):");
+        
+        // guarda la cilindrada como número entero
+        int cilindrada = scanner.nextInt();
+        
+        // se solicita la capacidad de pasajeros
+        System.out.println("Ingrese la capacidad de pasajeros:");
+        
+        // guarda la capacidad de pasajeros como número entero
+        int capacidadPasajeros = scanner.nextInt();
+
+        scanner.close();
+    }
+}
